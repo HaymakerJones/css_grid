7398b33f3cdbbb8e23eb2669814e519439febc9e
HaymakerJones <js388111@gmail.com>
2018-03-27 09:28:01 -0400

    demo 1 without custom breakpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { Demo1Component } from './demo1/demo1.component';
+
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo1Component
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ScrollPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
